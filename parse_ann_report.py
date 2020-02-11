2532e56bff0c12d73f4c68f56384dd049d3197a3
Julia Eskew <jeskew@edx.org>
2020-02-11 14:30:40 -0500

    Initial commit of parsing script

---
+++ parse_ann_report.py
@@ -0,0 +1,46 @@
+import click
+from yaml import load, dump
+
+NO_PII_ANNOTATION = '.. no_pii:'
+PII_TYPES_ANNOTATION = '.. pii_types:'
+
+
+def parse_pii_data(infile):
+    pii_models = {}
+    with open(infile, 'r') as pii_file:
+        pii_data = load(pii_file)
+    for filename in pii_data:
+        for annotation in pii_data[filename]:
+            if NO_PII_ANNOTATION in annotation['annotation_token']:
+                continue
+            elif PII_TYPES_ANNOTATION in annotation['annotation_token']:
+                if filename in pii_models:
+                    pii_models[filename].append(annotation)
+                else:
+                    pii_models[filename] = [annotation]
+    return pii_models
+
+def generate_report(pii_data, outfile):
+    with open(outfile, 'w') as pii_outfile:
+        dump(pii_data, pii_outfile)
+
+@click.command()
+@click.option('--infile',
+              default='edxapp_pii_report.yaml',
+              help="YAML file - the output of the PII annotation report.",
+              required=False
+              )
+@click.option('--outfile',
+              default='pii_models.txt',
+              help="TXT file to which to output all Django models w/PII.",
+              required=False
+              )
+def cli(infile, outfile):
+    """
+    Parses and analyzes paths to tracking log files.
+    """
+    pii_annotation_data = parse_pii_data(infile)
+    generate_report(pii_annotation_data, outfile)
+
+if __name__ == '__main__':
+    cli()  # pylint: disable=no-value-for-parameter
