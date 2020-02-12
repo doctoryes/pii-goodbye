4dfa003814554306991ae7ad40a6273cd49cbea6
Julia Eskew <jeskew@edx.org>
2020-02-12 16:32:18 -0500

    Quote strings properly and remove PyYaml warning.

--- output_view_redactions.py
+++ output_view_redactions.py
@@ -7,23 +7,28 @@ the redactions.yml file in the warehouse-transforms repo.
 """
 
 import click
-from yaml import load, dump
+import yaml
 
 def parse_pii_data(infile):
     pii_redactions = {}
     with open(infile, 'r') as pii_file:
-        pii_data = load(pii_file)
+        pii_data = yaml.load(pii_file, Loader=yaml.FullLoader)
     for filename in pii_data:
         for model in pii_data[filename]:
             table_name = model['db_table_name']
             pii_fields = model['pii_fields']
-            schema_table = '{app_name}.{table_name}'.format(app_name='LMS', table_name=table_name)
-            pii_redactions[schema_table] = [pii_fields]
+            schema_table = '{app_name}.{table_name}'.format(app_name='LMS', table_name=table_name.upper())
+            redactions = {}
+            for field, val in pii_fields.items():
+                if isinstance(val, str):
+                    val = "'{}'".format(val)
+                redactions[field.upper()] = val
+            pii_redactions[schema_table] = redactions
     return pii_redactions
 
 def generate_redaction_yml(pii_data, outfile):
     with open(outfile, 'w') as redactions_file:
-        dump(pii_data, redactions_file)
+        yaml.dump(pii_data, redactions_file)
 
 
 @click.command()
